3f7e2518b0e4408b0aef6aed4d7fa8fe688c83eb
cindytran211 <Cindy795Tran@gmail.com>
2023-12-11 14:20:28 -0500

    adding js to like/unlike button

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function performSearch(){
     var searchTerm=document.getElementById("searchInput").value;
-    window.location.href = "/"+searchTerm+"review.html";
+    if(searchTerm!="poke" && searchTerm!="mcdonald"){
+        window.location.href = "/unknown.html";
+    }else{
+        window.location.href = "/"+searchTerm+"review.html";
+    }
+}
+
+function changeImageSource(id){
+    var imageElement = document.getElementById(id);
+    if (imageElement.src.endsWith("fullheart.png")) {
+        imageElement.src = "images/blankheart.png";
+    } else {
+        imageElement.src = "images/fullheart.png";
+    }
 }
